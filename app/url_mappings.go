a3997a1c8a0a4828b0379ad067f078e8d74288bd
kento75 <kento2github@gmail.com>
2021-05-18 08:01:08 +0900

    refactor

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,12 +1,13 @@
 package app
 
 import (
-	"github.com/go-sandbox/bookstore_users-api/controllers"
+	"github.com/go-sandbox/bookstore_users-api/controllers/ping"
+	"github.com/go-sandbox/bookstore_users-api/controllers/users"
 )
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	router.GET("/ping", ping.Ping)
 
-	router.GET("/users/:user_id", controllers.GetUser)
-	router.POST("/users", controllers.CreateUser)
+	router.GET("/users/:user_id", users.GetUser)
+	router.POST("/users", users.CreateUser)
 }
