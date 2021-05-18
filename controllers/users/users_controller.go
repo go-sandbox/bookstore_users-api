872de02e104a0fff9a5a9fd9fe6aa064c738014d
kento75 <kento2github@gmail.com>
2021-05-18 21:45:23 +0900

    add create route

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-sandbox/bookstore_users-api/domain/users"
+	"github.com/go-sandbox/bookstore_users-api/services"
+	"github.com/go-sandbox/bookstore_users-api/utils/errors"
 )
 
 var (
@@ -11,7 +14,24 @@ var (
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+
+	// フォーマットエラー
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+
+	// 登録エラー
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,15 @@
-package 
+package services
+
+import (
+	"net/http"
+
+	"github.com/go-sandbox/bookstore_users-api/domain/users"
+	"github.com/go-sandbox/bookstore_users-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+
+	return &user, &errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+}
